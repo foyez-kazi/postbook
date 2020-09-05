57cf2170162c76053ca239e6fad09e71a6044c7d
foyez <foyezar@gmail.com>
2020-09-05 19:37:14 +0600

    Updates

---
+++ precache-manifest.28e49c44a60bc9b48ac80dea423ccecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cf125807e106fee894c90fd70be9f0",
+    "url": "/postbook/index.html"
+  },
+  {
+    "revision": "59131ed6172c197694b3",
+    "url": "/postbook/static/js/2.feb1e159.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/postbook/static/js/2.feb1e159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a123c00a866568fb80",
+    "url": "/postbook/static/js/main.5ec0b3d0.chunk.js"
+  },
+  {
+    "revision": "5ee0124191b44e12d895",
+    "url": "/postbook/static/js/runtime-main.0b5f4137.js"
+  }
+]);
